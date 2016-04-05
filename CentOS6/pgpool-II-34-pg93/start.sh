d552998865c2193460573232522b7cceba6dcaa2
Yugo Nagata <nagata@sraoss.co.jp>
2016-04-05 16:48:04 +0900

    Revert "Fix Dockerfile to handle muptiple versions of pgpool and postgres"
    This reverts commit 71dc4b917445eb761e1b7a5e1c9c6bce77ba4cc5.

---
+++ CentOS6/pgpool-II-34-pg93/start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# 1GB shared memory
+umount /dev/shm
+mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
+sysctl -w kernel.shmmax=1063256064
+sysctl -w kernel.shmall=259584
+ipcs -l
+service sshd start
+if [ ! -d /var/volum/$PGPOOL_BRANCH ];then
+    mkdir /var/volum/$PGPOOL_BRANCH
+fi
+service memcached start
+chown -R postgres /var/volum/$PGPOOL_BRANCH
+su postgres < /tmp/regress.sh
